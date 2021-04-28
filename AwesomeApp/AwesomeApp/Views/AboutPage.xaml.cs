bd2f3b331a93ed48affd92bc2a9518f67ab4c8fc
marsellaeve <marsella.eve@gmail.com>
2021-04-28 17:32:41 +0700

    awesome app

---
+++ AwesomeApp/AwesomeApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AwesomeApp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+    }
+}
---
+++ AwesomeApp/AwesomeApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AwesomeApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AwesomeApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
